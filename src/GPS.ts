5c9cf7ea1b1524c3ba194ed7d7a42496e279d48d
Gabriel <gabriel.bejarano@digitalcomtech.com>
2020-03-02 14:50:55 -0500

    filtering threshold to prevent weird change of bearing

--- src/GPS.ts
+++ src/GPS.ts
@@ -4,20 +4,20 @@
  */
 import * as Redis from "ioredis";
 import utils from "./Utils";
-var redis = new Redis();
 const MAX_TRIES = 3;
+const SPEED_THRESHOLD = 3;
+var redis = new Redis();
 var tries = 0;
-
 function rawdataToCoordinates(raw: string) {
 	var gps = JSON.parse(raw);
 	return {
 		coords: {
 			latitude: gps.lat,
 			longitude: gps.lon,
-			speed: gps.speed,
+			speed: gps.speed >= SPEED_THRESHOLD ? gps.speed : 0,
 			accuracy: 5 * gps.hdop,
 			altitude: gps.alt,
-			bearing: gps.track,
+			bearing: gps.speed >= SPEED_THRESHOLD ? gps.track : 0,
 			altitudeAccuracy: 5 * gps.vdop
 		},
 		timestamp: new Date(gps.time).getTime() / 1000,
