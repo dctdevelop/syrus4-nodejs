b115cc8905b8fa99c3cb05883aea64ea3fc5df8b
Juan Gomez <jgomez89@me.com>
2022-04-14 10:40:09 -0400

    double function cleanup

--- src/RFID.ts
+++ src/RFID.ts
@@ -37,7 +37,7 @@ export class RFIDUpdate{
 
     public digest( event: RFIDEvent ) {
         this.last = event;
-        
+
         if ( event.whitelisted ) {
             this.authorized.last = event;
         }
@@ -48,7 +48,7 @@ export class RFIDUpdate{
 
 // TODO: Check getAll()
 export function getAll(): Promise<RFIDEvent[]> {
-  return Utils.OSExecute(`apx-serial-rfid list`);  
+  return Utils.OSExecute(`apx-serial-rfid list`);
 }
 
 export function getLast(): Promise<RFIDEvent>{
@@ -59,57 +59,49 @@ export function clearLast(): Promise<RFIDEvent>{
   return Utils.OSExecute(`apx-serial-rfid clear --last`);
 }
 
-export function add(id: string, alias: string): Promise<void>{
+export function setRFIDAlias(id: string, alias: string): Promise<void>{
   if(alias == "") throw "Alias Name is required";
   if(id == "") throw "RFID id is required";
   return Utils.OSExecute(`apx-serial set --id=${id} --alias=${alias}`);
 }
 
-export function changeAlias(id: string, alias: string): Promise<void>{
-  if(alias == "") throw "Alias Name is required";
-  if(id == "") throw "RFID id is required";
-  return Utils.OSExecute(`apx-serial set --id=${id} --alias=${alias}`);
-}
-
-export function remove(id: string): Promise<void>{
+export function removeAlias(id: string): Promise<void>{
   if(id == "") throw "Id is required";
   return Utils.OSExecute(`apx-serial-rfid remove --id=${id}`);
 }
 
 export function removeAll(): Promise<void>{
-    return Utils.OSExecute('apx-serial-rfid remove --all');
+  return Utils.OSExecute('apx-serial-rfid remove --all');
 }
 
 export async function onRFIDEvent( callback:(arg: RFIDUpdate) => void, errorCallback:(arg: Error) => void) : Promise<{ unsubscribe: () => void, off: () => void}> {
   const topic = "serial/notification/rfid/state";
-    // GET last RFID data
-    let rfid_update = new RFIDUpdate()
-    let last_rfid_event = await getLast().catch(console.error)
-    if(last_rfid_event) {
-        callback(rfid_update.digest(last_rfid_event))
-    }
-
-    // Subscribe to receive updates
-    try {
-        var state: RFIDEvent;
-        var handler = (channel: string, data: any) => {
-        if (channel != topic) return
-            state = JSON.parse(data);
-            callback(rfid_update.digest(state))
-        };
-        subscriber.subscribe(topic);
-        subscriber.on("message", handler);
-    } catch (error) {
-        console.error('onRFIDEvent error:', error);
-        errorCallback(error);
-    }
-    let returnable = {
+  // GET last RFID data
+  let rfid_update = new RFIDUpdate()
+  let last_rfid_event = await getLast().catch(console.error)
+  if(last_rfid_event) {
+    callback(rfid_update.digest(last_rfid_event))
+  }
+  // Subscribe to receive updates
+  try {
+    var state: RFIDEvent;
+    var handler = (channel: string, data: any) => {
+    if (channel != topic) return
+      state = JSON.parse(data);
+      callback(rfid_update.digest(state))
+    };
+    subscriber.subscribe(topic);
+    subscriber.on("message", handler);
+  } catch (error) {
+    console.error('onRFIDEvent error:', error);
+    errorCallback(error);
+  }
+  let returnable = {
     unsubscribe: () => {
-        subscriber.off("message", handler);
-        subscriber.unsubscribe(topic);
+      subscriber.off("message", handler);
+      subscriber.unsubscribe(topic);
     },
     off: function () { this.unsubscribe() }
   };
-
   return returnable;
 }
